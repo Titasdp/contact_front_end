19538fc0b82337ed21eb234f8aaab71122416d4f
Titasdp <tiagopina20014@gmail.com>
2023-10-06 03:07:07 +0100

    log out process added

--- src/pages/dashboardHome.js
+++ src/pages/dashboardHome.js
@@ -4,8 +4,10 @@ import {
   check_login,
   force_password_change,
 } from "../utils/navigationRules/navigationRulesCheck";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { add_value } from "../utils/storage/loggedUserSlice";
 export default function DashboardHome() {
+  const dispatch = useDispatch();
   let logged_user_info = useSelector((state) => state.loggedUser.value);
   const navigate = useNavigate();
 
@@ -20,6 +22,19 @@ export default function DashboardHome() {
     navigate("/contacts");
   };
 
+  const log_out = async () => {
+    await dispatch(
+      add_value({
+        user_token: "",
+        user_id: "",
+        user_contacts: [],
+        user_information: "",
+      })
+    );
+
+    navigate("/login");
+  };
+
   useEffect(() => {
     if (!check_login(logged_user_info)) {
       navigate("/login");
@@ -30,36 +45,52 @@ export default function DashboardHome() {
   }, []);
 
   return (
-    <div
-      className="d-flex justify-content-center align-items-center"
-      style={{ height: "100vh" }}
-    >
-      <div className="text-center">
-        <h1 className="">WELCOME TO THE DASHBOARD</h1>
-        <p className="">
-          This is our app dashboard please select one of the option bellow:
+    <>
+      <div className="container-fluid  d-flex justify-content-start align-items-center flex-row">
+        <button
+          className="btn btn-outline-secondary mr-2"
+          onClick={function () {
+            log_out();
+          }}
+        >
+          LOG OUT
+        </button>
+
+        <p style={{ padding: "10px 0px", margin: "0px 0px 0px 10px" }}>
+          /dashboard
         </p>
-        <div className="d-flex justify-content-between">
-          <button
-            className="btn btn-outline-secondary mr-2"
-            onClick={move_to_profile}
-          >
-            MY PROFILE
-          </button>
-          <button
-            className="btn btn-outline-secondary mr-2"
-            onClick={move_to_password_manager}
-          >
-            PASSWORD
-          </button>
-          <button
-            className="btn btn btn-outline-secondary mr-2"
-            onClick={move_to_contact_dashboard}
-          >
-            MY CONTACTS
-          </button>
+      </div>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="text-center">
+          <h1 className="">WELCOME TO THE DASHBOARD</h1>
+          <p className="">
+            This is our app dashboard please select one of the option bellow:
+          </p>
+          <div className="d-flex justify-content-between">
+            <button
+              className="btn btn-outline-secondary mr-2"
+              onClick={move_to_profile}
+            >
+              MY PROFILE
+            </button>
+            <button
+              className="btn btn-outline-secondary mr-2"
+              onClick={move_to_password_manager}
+            >
+              PASSWORD
+            </button>
+            <button
+              className="btn btn btn-outline-secondary mr-2"
+              onClick={move_to_contact_dashboard}
+            >
+              MY CONTACTS
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
